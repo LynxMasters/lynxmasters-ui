c22a19240fec7c40da0b9ef8b328ed5607b55983
nmonty9 <namontalbano@email.neit.edu>
2018-07-25 14:43:20 -0400

    added linkaccounts.vue

---
+++ src/components/LinkAccounts.vue
@@ -0,0 +1,36 @@
+<template>
+   <div>
+            
+           
+            <button v-on:click="reddit" class="button--reddit">Auth reddit</button>
+            <button v-on:click="twitch" class="button--twitch">Auth twitch</button>
+            <button v-on:click="twitter" class="button--twitter">Auth twitter</button>   
+                
+  </div>
+</template>
+<script>
+import _ from 'lodash'
+export default{
+        methods:{
+
+          twitter: function() {
+            alert("pressed");
+            window.location = 'http://localhost:9999/auth/twitter';
+          },
+          reddit: function() {
+            alert("pressed");
+            window.location = 'http://localhost:9999/auth/reddit';
+          },
+          twitch: function() {
+            alert("pressed");
+            window.location = 'http://localhost:9999/auth/twitch';
+          }
+        }
+}        
+</script>
+
+<style scoped type="scss">
+  .no-display {
+    display: block!important;
+  }
+</style>
