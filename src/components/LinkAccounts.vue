09cabfded5c190b6dd9c2a9e1c4c20960d704855
ianarsenault <ian_arsenault@protonmail.com>
2018-07-26 21:39:12 -0400

    #27: updated link accounts component

--- src/components/LinkAccounts.vue
+++ src/components/LinkAccounts.vue
@@ -1,36 +1,99 @@
 <template>
-   <div>
-            
-           
-            <button v-on:click="reddit" class="button--reddit">Auth reddit</button>
-            <button v-on:click="twitch" class="button--twitch">Auth twitch</button>
-            <button v-on:click="twitter" class="button--twitter">Auth twitter</button>   
-                
-  </div>
+  <!-- Social Add-Ons -->
+  <section class="container">
+    <div class="intro column is-8 is-offset-2">
+      <h2 class="title">Link Accounts</h2><hr>
+    </div>
+    <div id="social" class="columns features">
+      <div class="column is-4">
+        <div class="card is-shady card-equal-height">
+          <div class="card-image has-text-centered">
+            <i class="fab fa-twitter fa-7x"></i>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h4 class="has-text-centered">Twitter</h4>
+              <p>Twitter is an online news and social networking service on which users post and interact with messages known as "tweets".</p>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a href="#" class="card-footer-item is-primary button"
+               @click="twitter">Link Twitter Account</a>
+          </footer>
+        </div>
+      </div>
+      <div class="column is-4">
+        <div class="card is-shady card-equal-height">
+          <div class="card-image has-text-centered">
+            <i class="fab fa-twitch fa-7x"></i>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h4 class="has-text-centered">Twitch</h4>
+                <p>The community where millions of people and thousands of interests collide in a beautiful explosion of video games, pop culture, and conversation. With chat built into every stream, you don’t just watch on Twitch, you’re a part of the show. From classic tv show marathons to esports tournaments, if you can imagine it, it’s probably live on Twitch right now.
+                </p>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a class="card-footer-item is-primary button"
+               @click="twitch">Link Twitch Account</a>
+          </footer>
+        </div>
+      </div>
+      <div class="column is-4">
+        <div class="card is-shady card-equal-height">
+          <div class="card-image has-text-centered">
+            <i class="fab fa-reddit fa-7x"></i>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h4 class="has-text-centered">Reddit</h4>
+              <p>Reddit is home to thousands of communities, endless conversation, and authentic human connection. Whether you're into breaking news, sports, TV fan theories, or a never-ending stream of the internet's cutest animals, there's a community on Reddit for you.</p>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a class="card-footer-item button is-primary"
+               @click="reddit">Link Reddit Account</a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Social Add-Ons -->
 </template>
-<script>
-import _ from 'lodash'
-export default{
-        methods:{
 
-          twitter: function() {
-            alert("pressed");
-            window.location = 'http://localhost:9999/auth/twitter';
-          },
-          reddit: function() {
-            alert("pressed");
-            window.location = 'http://localhost:9999/auth/reddit';
-          },
-          twitch: function() {
-            alert("pressed");
-            window.location = 'http://localhost:9999/auth/twitch';
-          }
-        }
-}        
+<script>
+  // import services here...
+  import UserService from '@/services/UserService'
+
+  export default {
+    name: 'LinkAccounts',
+    data() {
+      return {}
+    },
+    watch: {},
+    computed: {},
+    methods:{
+      twitter: function() {
+        window.location = 'http://localhost:9999/auth/twitter'
+      },
+      reddit: function() {
+        window.location = 'http://localhost:9999/auth/reddit'
+      },
+      twitch: function() {
+        window.location = 'http://localhost:9999/auth/twitch'
+      }
+    }
+  }
 </script>
 
 <style scoped type="scss">
-  .no-display {
-    display: block!important;
+  #social .column .card {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  #social .column .card-footer {
+    margin-top: auto;
   }
 </style>
