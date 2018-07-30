65378472db3e26033cdd1eeb5dc230745c549d36
Jameson Arsenault <jamesonmarsenault@gmail.com>
2018-07-30 19:08:15 -0400

    #24: Added scrollspy to div, and to package using vue2-scrollspy

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import router from './router'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
 import VueScrollTo from 'vue-scrollto'
+import scrollSpy from 'vue2-scrollspy';
 import Bulma from 'bulma/css/bulma.css'
 import './assets/style/app.scss'
 import store from './store/store'
 
 Vue.use(Buefy)
+Vue.use(scrollSpy)
 Vue.use(VueScrollTo)
 
 Vue.config.productionTip = false
