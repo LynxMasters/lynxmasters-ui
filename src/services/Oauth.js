29f91b61b2c596c4bd4accd04d138912f0e90cb2
nmonty9 <namontalbano@email.neit.edu>
2018-08-02 14:24:54 -0400

    unsupported_grant_type reddit callback

--- src/services/Oauth.js
+++ src/services/Oauth.js
@@ -3,20 +3,16 @@ import axios from 'axios'
 export default {
   
   authReddit(code) {
+   let auth = 'h9NwYUZGn65RJw : vOGJ1itvyfWHE_ZxiA5kYKGWl-Q';
   	let config = {
   		headers: {
-  			
-  			'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin',
-  			'Access-Control-Allow-Origin': 'http://localhost:8080',
-    		'Authorization': 'Basic h9NwYUZGn65RJw:vOGJ1itvyfWHE_ZxiA5kYKGWl-Q'
+  		    'Content-Type': 'application/x-www-form-urlencoded',
+    		'Authorization': 'Basic aDlOd1lVWkduNjVSSnc6dk9HSjFpdHZ5ZldIRV9aeGlBNWtZS0dXbC1R'
     	    
   		}
   	}
-	let data = {
-  		grant_type: 'authorizationcode',
-    	code: code,
-    	redirect_uri: 'http://localhost:8080/LinkAccounts'
-	}
+	let data = 'grant_type= authorization_code&code='+code+'&redirect_uri=http://localhost:8080/LinkAccounts'
+	
 	let url = 'https://www.reddit.com/api/v1/access_token'
 
     axios.post(url, data, config);
