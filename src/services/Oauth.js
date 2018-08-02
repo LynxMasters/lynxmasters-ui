e468e33271ed2998609eddc9474ad89a6cd10d7f
nmonty9 <namontalbano@email.neit.edu>
2018-08-02 14:31:06 -0400

    reddit working

--- src/services/Oauth.js
+++ src/services/Oauth.js
@@ -6,12 +6,13 @@ export default {
    let auth = 'h9NwYUZGn65RJw : vOGJ1itvyfWHE_ZxiA5kYKGWl-Q';
   	let config = {
   		headers: {
+  			'Accept': 'application/x-www-form-urlencoded',	
   		    'Content-Type': 'application/x-www-form-urlencoded',
     		'Authorization': 'Basic aDlOd1lVWkduNjVSSnc6dk9HSjFpdHZ5ZldIRV9aeGlBNWtZS0dXbC1R'
     	    
   		}
   	}
-	let data = 'grant_type= authorization_code&code='+code+'&redirect_uri=http://localhost:8080/LinkAccounts'
+	let data = 'grant_type=authorization_code&code='+code+'&redirect_uri=http://localhost:8080/LinkAccounts'
 	
 	let url = 'https://www.reddit.com/api/v1/access_token'
 
