a31572e54b7f722b3311e314ab6b0970677644b4
ianarsenault <ian_arsenault@protonmail.com>
2018-08-09 22:58:14 -0400

    add reddit card

--- src/components/LinkAccounts.vue
+++ src/components/LinkAccounts.vue
@@ -71,7 +71,10 @@
           </div>
           <div class="card-content">
             <div class="content">
-              <h4 class="has-text-centered">Reddit Username = </h4>
+              <h4 class="has-text-centered">@{{ accounts.reddit.name }}</h4>
+              <h5 class="has-text-centered">{{ accounts.reddit.link_karma }} Post Karma</h5>
+              <h5 class="has-text-centered">{{ accounts.reddit.comment_karma }} Comment Karma</h5>
+              <h5 class="has-text-centered">Date Created: {{ moment.unix(accounts.reddit.created).format('YYYY-MM-DD') }}</h5>
             </div>
           </div>
           <footer class="card-footer">
