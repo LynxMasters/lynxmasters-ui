1073c4021937944c8032b5747d358099e3814f02
ianarsenault <ian_arsenault@protonmail.com>
2018-08-11 12:38:36 -0400

    completed card transitions

--- src/components/LinkAccounts.vue
+++ src/components/LinkAccounts.vue
@@ -1,15 +1,11 @@
 <template>
   <!-- Social Add-Ons -->
   <section class="container">
-
     <div class="intro column is-8 is-offset-2">
       <h2 class="title">Link Accounts</h2><hr>
       <!--<h1>twitter is loaded = {{ isLoaded.twitter }} and twitter is linked = {{ isLoaded.hasTwitterLinked }}</h1>-->
-      <!--<h1>twitch is loaded = {{ isLoaded.twitch }}</h1>-->
-      <!--<h1>reddit is loaded = {{ isLoaded.reddit }}</h1>-->
-      <!--<transition name="fade" >-->
-        <!--<p v-if=" isLoaded.hasTwitterLinked"><h1>HELLO THERE</h1></p>-->
-      <!--</transition>-->
+      <!--<h1>twitch is loaded = {{ isLoaded.twitch }} and twitch is linked = {{ isLoaded.hasTwitchLinked }}</h1>-->
+      <!--<h1>reddit is loaded = {{ isLoaded.reddit }} and REDDIT is linked = {{ isLoaded.hasRedditLinked }}</h1>-->
     </div>
     <div id="social" class="columns features">
       <!-- BEGIN TWITTER CARDS -->
@@ -89,198 +85,155 @@
 
 
       <!-- BEGIN TWITCH CARDS -->
-      <div class="column is-4"
-           v-if="!isLoaded.twitch">
-        <div class="card is-shady card-equal-height">
-          <header class="card-header">
-            <p class="card-header-title is-centered">
-              Checking for Twitch
-            </p>
-          </header>
-          <div class="card-content">
-            <div class="content has-text-centered">
-              <loading-progress
-                :progress="progress"
-                :indeterminate="indeterminate"
-                :counter-clockwise="counterClockwise"
-                :hide-background="hideBackground"
-                size="64"
-                rotate
-                fillDuration="2"
-                rotationDuration="1"
-              />
+      <transition name="fade" mode="out-in">
+        <div class="column is-4"
+             v-if="!isLoaded.twitch">
+          <div class="card is-shady card-equal-height">
+            <header class="card-header">
+              <p class="card-header-title is-centered">
+                Checking for Twitch
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                <loading-progress
+                  :progress="progress"
+                  :indeterminate="indeterminate"
+                  :counter-clockwise="counterClockwise"
+                  :hide-background="hideBackground"
+                  size="64"
+                  rotate
+                  fillDuration="2"
+                  rotationDuration="1"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="column is-4"
-           v-if="!isLoaded.reddit">
-        <div class="card is-shady card-equal-height">
-          <header class="card-header">
-            <p class="card-header-title is-centered">
-              Checking for Reddit
-            </p>
-          </header>
-          <div class="card-content">
-            <div class="content has-text-centered">
-              <loading-progress
-                class="progress-twitter"
-                :progress="progress"
-                :indeterminate="indeterminate"
-                :counter-clockwise="counterClockwise"
-                :hide-background="hideBackground"
-                size="64"
-                rotate
-                fillDuration="2"
-                rotationDuration="1"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    <!--</div>-->
-
-
-    <!--<div id="social" class="columns features">-->
-
-
-      <!--<div class="column is-4">-->
-        <!--<div class="card is-shady card-equal-height">-->
-          <!--<header class="card-header">-->
-            <!--<p class="card-header-title">-->
-              <!--<i class="fab fa-twitter fa-2x linked"></i>-->
-              <!--<span>-->
-                <!--Twitter Linked-->
-              <!--</span>-->
-            <!--</p>-->
-          <!--</header>-->
-          <!--<div class="card-image has-text-centered">-->
-            <!--<img :src="twitterImg" class="twitter-img">-->
-          <!--</div>-->
-          <!--<div class="card-content">-->
-            <!--<div class="content">-->
-              <!--<h4 class="has-text-centered">@{{ accounts.twitter.screen_name }}</h4>-->
-              <!--<h5 class="has-text-centered">Followers: {{ accounts.twitter.followers_count }} Following:  {{ accounts.twitter.friends_count }}</h5>-->
-              <!--<h5 class="has-text-centered">Date Created: {{ moment(accounts.twitter.created_at).format('YYYY-MM-DD') }}</h5>-->
-            <!--</div>-->
-          <!--</div>-->
-          <!--<footer class="card-footer">-->
-            <!--<a href="#" class="card-footer-item is-light button"-->
-               <!--@click="">Unlink Twitter Account</a>-->
-          <!--</footer>-->
-        <!--</div>-->
-      <!--</div>-->
-      <div class="column is-4">
-        <div class="card is-shady card-equal-height">
-          <header class="card-header">
-            <p class="card-header-title">
-              <i class="fab fa-twitch fa-2x linked"></i>
-              <span>
+        <div class="column is-4 animated fadeIn"
+             v-if="isLoaded.twitch && isLoaded.hasTwitchLinked">
+          <div class="card is-shady card-equal-height">
+            <header class="card-header">
+              <p class="card-header-title">
+                <i class="fab fa-twitch fa-2x linked"></i>
+                <span>
                 Twitch Linked
               </span>
-            </p>
-          </header>
-          <div class="card-image has-text-centered">
-            <img :src="accounts.twitch.logo">
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4 class="has-text-centered">@{{ accounts.twitch.display_name }}</h4>
-              <h5 class="has-text-centered">Bio: {{ accounts.twitch.bio }}</h5>
-              <h5 class="has-text-centered">Date Created: {{ moment(accounts.twitch.created_at).format('YYYY-MM-DD') }}</h5>
+              </p>
+            </header>
+            <div class="card-image has-text-centered">
+              <img :src="accounts.twitch.logo">
             </div>
-          </div>
-          <footer class="card-footer">
-            <a class="card-footer-item is-light button"
-               @click="">Unlink Twitch Account</a>
-          </footer>
-        </div>
-      </div>
-      <div class="column is-4">
-        <div class="card is-shady card-equal-height">
-          <header class="card-header">
-            <p class="card-header-title">
-              <i class="fab fa-reddit fa-2x linked"></i>
-              <span>
-                Reddit Linked
-              </span>
-            </p>
-          </header>
-          <div class="card-image has-text-centered">
-            <img :src="accounts.reddit.icon_img">
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4 class="has-text-centered">@{{ accounts.reddit.name }}</h4>
-              <h5 class="has-text-centered">{{ accounts.reddit.link_karma }} Post Karma</h5>
-              <h5 class="has-text-centered">{{ accounts.reddit.comment_karma }} Comment Karma</h5>
-              <h5 class="has-text-centered">Date Created: {{ moment.unix(accounts.reddit.created).format('YYYY-MM-DD') }}</h5>
+            <div class="card-content">
+              <div class="content">
+                <h4 class="has-text-centered">@{{ accounts.twitch.display_name }}</h4>
+                <h5 class="has-text-centered">Bio: {{ accounts.twitch.bio }}</h5>
+                <h5 class="has-text-centered">Date Created: {{ moment(accounts.twitch.created_at).format('YYYY-MM-DD') }}</h5>
+              </div>
             </div>
+            <footer class="card-footer">
+              <a class="card-footer-item is-light button"
+                 @click="">Unlink Twitch Account</a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item button is-light"
-               @click="">Unlink Reddit Account</a>
-          </footer>
         </div>
-      </div>
-    <!--</div>-->
-
-    <!--<div id="social" class="columns features">-->
-
-      <!--<div class="column is-4">-->
-        <!--<div class="card is-shady card-equal-height">-->
-          <!--<div class="card-image has-text-centered">-->
-            <!--<i class="fab fa-twitter fa-7x"></i>-->
-          <!--</div>-->
-          <!--<div class="card-content">-->
-            <!--<div class="content">-->
-              <!--<h4 class="has-text-centered">Twitter</h4>-->
-              <!--<p>Twitter is an online news and social networking service on which users post and interact with messages known as "tweets".</p>-->
-            <!--</div>-->
-          <!--</div>-->
-          <!--<footer class="card-footer">-->
-            <!--<a href="#" class="card-footer-item is-primary button"-->
-               <!--@click="twitter">Link Twitter Account</a>-->
-          <!--</footer>-->
-        <!--</div>-->
-      <!--</div>-->
-
-
-      <div class="column is-4">
-        <div class="card is-shady card-equal-height">
-          <div class="card-image has-text-centered">
-            <i class="fab fa-twitch fa-7x"></i>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4 class="has-text-centered">Twitch</h4>
+        <div class="column is-4 animated fadeIn"
+             v-if="isLoaded.twitch && !isLoaded.hasTwitchLinked">
+          <div class="card is-shady card-equal-height">
+            <div class="card-image has-text-centered">
+              <i class="fab fa-twitch fa-7x"></i>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                <h4 class="has-text-centered">Twitch</h4>
                 <p>The community where millions of people and thousands of interests collide in a beautiful explosion of video games, pop culture, and conversation. With chat built into every stream, you don’t just watch on Twitch, you’re a part of the show. From classic tv show marathons to esports tournaments, if you can imagine it, it’s probably live on Twitch right now.
                 </p>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a class="card-footer-item is-primary button"
+                 @click="twitch">Link Twitch Account</a>
+            </footer>
+          </div>
+        </div>
+      </transition>
+      <!-- END TWITCH CARDS -->
+
+      <!--  BEGIN REDDIT CARDS -->
+      <transition name="fade" mode="out-in">
+        <div class="column is-4"
+             v-if="!isLoaded.reddit">
+          <div class="card is-shady card-equal-height">
+            <header class="card-header">
+              <p class="card-header-title is-centered">
+                Checking for Reddit
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-centered">
+                <loading-progress
+                  class="progress-twitter"
+                  :progress="progress"
+                  :indeterminate="indeterminate"
+                  :counter-clockwise="counterClockwise"
+                  :hide-background="hideBackground"
+                  size="64"
+                  rotate
+                  fillDuration="2"
+                  rotationDuration="1"
+                />
+              </div>
             </div>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item is-primary button"
-               @click="twitch">Link Twitch Account</a>
-          </footer>
         </div>
-      </div>
-      <div class="column is-4">
-        <div class="card is-shady card-equal-height">
-          <div class="card-image has-text-centered">
-            <i class="fab fa-reddit fa-7x"></i>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <h4 class="has-text-centered">Reddit</h4>
-              <p>Reddit is home to thousands of communities, endless conversation, and authentic human connection. Whether you're into breaking news, sports, TV fan theories, or a never-ending stream of the internet's cutest animals, there's a community on Reddit for you.</p>
+        <div class="column is-4 animated fadeIn"
+             v-if="isLoaded.reddit && isLoaded.hasRedditLinked">
+          <div class="card is-shady card-equal-height">
+            <header class="card-header">
+              <p class="card-header-title">
+                <i class="fab fa-reddit fa-2x linked"></i>
+                <span>
+                Reddit Linked
+              </span>
+              </p>
+            </header>
+            <div class="card-image has-text-centered">
+              <img :src="accounts.reddit.icon_img">
             </div>
+            <div class="card-content">
+              <div class="content">
+                <h4 class="has-text-centered">@{{ accounts.reddit.name }}</h4>
+                <h5 class="has-text-centered">{{ accounts.reddit.link_karma }} Post Karma</h5>
+                <h5 class="has-text-centered">{{ accounts.reddit.comment_karma }} Comment Karma</h5>
+                <h5 class="has-text-centered">Date Created: {{ moment.unix(accounts.reddit.created).format('YYYY-MM-DD') }}</h5>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a class="card-footer-item button is-light"
+                 @click="">Unlink Reddit Account</a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a class="card-footer-item button is-primary"
-               @click="reddit">Link Reddit Account</a>
-          </footer>
         </div>
-      </div>
+        <div class="column is-4 animated fadeIn"
+             v-if="isLoaded.reddit && !isLoaded.hasRedditLinked">
+          <div class="card is-shady card-equal-height">
+            <div class="card-image has-text-centered">
+              <i class="fab fa-reddit fa-7x"></i>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                <h4 class="has-text-centered">Reddit</h4>
+                <p>Reddit is home to thousands of communities, endless conversation, and authentic human connection. Whether you're into breaking news, sports, TV fan theories, or a never-ending stream of the internet's cutest animals, there's a community on Reddit for you.</p>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a class="card-footer-item button is-primary"
+                 @click="reddit">Link Reddit Account</a>
+            </footer>
+          </div>
+        </div>
+      </transition>
+      <!-- END REDDIT CARDS -->
     </div>
   </section>
   <!-- End Social Add-Ons -->
@@ -328,31 +281,29 @@
           console.log(res)
           console.log('^^^^^^^^LynxMasters^^^^^^^^^^^')
 
+          let twitterInfo = {
+            endpoint: '/users/show.json?screen_name='+res.data.twitter.displayName,
+            oauth_token: res.data.twitter.oauth_token,
+            oauth_secret: res.data.twitter.oauth_secret,
+            jwt: window.localStorage.getItem('token'),
+            method: 'GET/' //If posting data method would be POST/
+          }
+
           if (!_.isEmpty(res.data.twitter.oauth_secret)) {
             this.isLoaded.hasTwitterLinked = true
-          }
-          if (!_.isEmpty(res.data.twitch.access_token )) {
-            this.isLoaded.hasTwitchLinked = true
-          }
-          if (!_.isEmpty(res.data.reddit.access_token)) {
-            this.isLoaded.hasRedditLinked = true
-          }
-
-          let redditInfo = {
-            endpoint: '/me',
-            access_token: res.data.reddit.access_token,
-            jwt: window.localStorage.getItem('token'),
-            method: 'GET/', //If posting data method would be POST/
-            user_agent: navigator.userAgent
+            ExternalService.twitterPOST(twitterInfo).then(response => {
+              return response
+            }).then((profile) => {
+              console.log(profile.data)
+              console.log('^^^^^^^^twitter^^^^^^^^^^^')
+              this.accounts.twitter = profile.data
+              this.normalizeImage(this.accounts.twitter.profile_image_url_https)
+              this.isLoaded.twitter = true
+            })
+          } else {
+            this.isLoaded.twitter = true
           }
 
-          ExternalService.redditPOST(redditInfo).then(response => {
-            return response
-          }).then((profile) => {
-            console.log(profile.data)
-            console.log('^^^^^^^^reddit^^^^^^^^^^^')
-            this.accounts.reddit = profile.data
-          })
 
           let twitchInfo = {
             endpoint: '/user?oauth_token='+res.data.twitch.access_token,
@@ -362,32 +313,43 @@
             user_agent: navigator.userAgent
           }
 
-          ExternalService.twitchPOST(twitchInfo).then(response => {
-            return response
-          }).then((profile) => {
-            console.log(profile.data)
-            console.log('^^^^^^^^twitch^^^^^^^^^^^')
-            this.accounts.twitch = profile.data
-          })
-
-          let twitterInfo = {
-            endpoint: '/users/show.json?screen_name='+res.data.twitter.displayName,
-            oauth_token: res.data.twitter.oauth_token,
-            oauth_secret: res.data.twitter.oauth_secret,
-            jwt: window.localStorage.getItem('token'),
-            method: 'GET/' //If posting data method would be POST/
+          if (!_.isEmpty(res.data.twitch.access_token )) {
+            console.log("NO TWITCH TOKEN")
+            this.isLoaded.hasTwitchLinked = true
+            ExternalService.twitchPOST(twitchInfo).then(response => {
+              return response
+            }).then((profile) => {
+              console.log(profile.data)
+              console.log('^^^^^^^^twitch^^^^^^^^^^^')
+              this.accounts.twitch = profile.data
+              this.isLoaded.twitch = true
+            })
+          } else {
+            this.isLoaded.twitch = true
           }
 
-          ExternalService.twitterPOST(twitterInfo).then(response => {
-            return response
-          }).then((profile) => {
-            console.log(profile.data)
-            console.log('^^^^^^^^twitter^^^^^^^^^^^')
-            this.accounts.twitter = profile.data
-            this.normalizeImage(this.accounts.twitter.profile_image_url_https)
-            this.isLoaded.twitter = true
 
-          })
+          let redditInfo = {
+            endpoint: '/me',
+            access_token: res.data.reddit.access_token,
+            jwt: window.localStorage.getItem('token'),
+            method: 'GET/', //If posting data method would be POST/
+            user_agent: navigator.userAgent
+          }
+
+          if (!_.isEmpty(res.data.reddit.access_token)) {
+            this.isLoaded.hasRedditLinked = true
+            ExternalService.redditPOST(redditInfo).then(response => {
+              return response
+            }).then((profile) => {
+              console.log(profile.data)
+              console.log('^^^^^^^^reddit^^^^^^^^^^^')
+              this.accounts.reddit = profile.data
+              this.isLoaded.reddit = true
+            })
+          } else {
+            this.isLoaded.reddit = true
+          }
 
         })
       },
