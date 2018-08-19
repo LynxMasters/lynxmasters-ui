33880b009b068c5fc5ad0c497107bbcb239854e7
ianarsenault <ian_arsenault@protonmail.com>
2018-08-19 14:32:05 -0400

    update font

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -2,7 +2,7 @@
   <section class="notFound">
     <div class="circles">
       <p>404<br>
-        <small>PAGE NOT FOUND</small>
+        <small class="pnf-text">PAGE NOT FOUND</small>
       </p>
       <span class="circle big"></span>
       <span class="circle med"></span>
