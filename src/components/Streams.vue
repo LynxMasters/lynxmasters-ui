4b34345409b96da8a39221fced2db72470e3e9db
nmonty9 <namontalbano@email.neit.edu>
2018-08-22 10:15:33 -0400

    removed unnecessary code

--- src/components/Streams.vue
+++ src/components/Streams.vue
@@ -1,28 +1,4 @@
 <template>
-<!-- <div>
-  <div class="columns medium-4">
-    <div class="card is-shady card-equal-height">
-      <header class="card-header">
-        <p class="card-header-title">
-          <span>
-            {{stream.channel.display_name}}
-          </span>
-          <i class="fab fa-twitch fa-2x"></i>
-        </p>
-      </header>
-      <div class="card-section">
-        <iframe v-bind:src="`http://player.twitch.tv/?channel=${stream.channel.name}&autoplay=false`"
-        height="500"
-        width="500"
-        frameborder="<frameborder>"
-        scrolling="<scrolling>"
-        allowfullscreen="<allowfullscreen>">
-          </iframe>
-        </div>
-      </div>
-    </div>
-  </div>
- -->
 <article class="media">
   <figure class="media-left">
   </figure>
