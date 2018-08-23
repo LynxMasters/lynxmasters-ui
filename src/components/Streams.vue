773c70a61ec461db2f054fb2ba0577d4513ef507
nmonty9 <namontalbano@email.neit.edu>
2018-08-22 21:18:15 -0400

    hide iframe function

--- src/components/Streams.vue
+++ src/components/Streams.vue
@@ -14,22 +14,24 @@
           scrolling="<scrolling>"
           allowfullscreen="<allowfullscreen>">
         </iframe>
-        <iframe frameborder="0"
-          scrolling="no"
-          id="chat_embed"
-          v-bind:src="`http://www.twitch.tv/embed/${stream.channel.name}/chat`"
-          height="500"
-          width="650">
-        </iframe>
+        <div v-bind:class="{'hideChat': !isChat}" >
+          <iframe frameborder="0"
+            scrolling="no"
+            id="chat_embed"
+            v-bind:src="`http://www.twitch.tv/embed/${stream.channel.name}/chat`"
+            height="500"
+            width="650">
+          </iframe>
+        </div>
       </p>
     </div>
     <nav class="level is-mobile">
       <div class="level-left">
-        <a class="level-item has-text-grey">
-          <span class="icon is-small"><i class="fas fa-comment"></i></span>
+        <a class="level-item has-text-grey" @click="chat">
+          <span class="icon is-small"><i class="fas fa-comment 2x"></i></span>
         </a>
         <a class="level-item has-text-grey">
-          <span class="icon is-small"><i class="fas fa-share"></i></span>
+          <span class="icon is-small"><i class="fas fa-share 2x"></i></span>
         </a>
       </div>
     </nav>
@@ -45,7 +47,26 @@
     props: {
      stream:{}
     },
+    data(){
+      return{
+        isChat: false,
+      }
+    },
     methods: {
+      chat(){
+        if(!this.isChat){
+          this.isChat = true
+        }else{
+          this.isChat = false
+        }
+      },
     },
   }
 </script> 
+<style scoped>
+  .hideChat{
+    visibility: hidden;
+    width: 0;
+    height: 0;
+  }
+</style>
